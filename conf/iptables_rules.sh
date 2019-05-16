282ec3c2720fb67326a7f8d3e9046b603ed52b39
socketz <socketz@users.noreply.github.com>
2019-05-17 01:50:01 +0200

    Fixed some issues with containers and configuration of iptables

--- conf/iptables_rules.sh
+++ conf/iptables_rules.sh
@@ -27,7 +27,7 @@
 ### 5: Block spoofed packets ###
 /sbin/iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
-/sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
+#/sbin/iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP # Disabled for docker incompatibility
 /sbin/iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
 /sbin/iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
